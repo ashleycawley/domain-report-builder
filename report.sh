b7a3ed378c6f28fec15b742946ab1a5d757f16f7
Ashley Cawley <ash@ashleycawley.co.uk>
2018-05-21 20:41:49 +0100

    init commit

---
+++ report.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+# Things I would like it to report:
+# - Website Type (WordPress, Drupal etc)
+# - Nameservers
+# - A Record
+# - MX Record
+# 
+
+# Variables
+
+# Functions
+function DNSTESTS {
+echo -e "Nameserver Records:"
+dig +short $DOMAIN NS && echo
+
+echo -e "A Record: \c"
+dig +short $DOMAIN A && echo
+
+echo -e "MX Record: \c"
+dig +short $DOMAIN MX && echo
+}
+
+function SCODELOOKUP {
+# Retrieves the status code from visiting $DOMAIN/wp-login.php and saves it into a variable called $STATUSCODE
+STATUSCODE=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://$DOMAIN/wp-login.php)
+}
+
+function LOOKUPLIC {
+# Checks to see if a licencse.txt file exists and whether it contains the word WordPress
+curl --silent http://$DOMAIN/license.txt | grep -q "WordPress"
+WPLIC=$(echo $?)
+}
+
+function LOOKUPWPINDEX {
+# Checks to see if a licencse.txt file exists and whether it contains the word WordPress
+curl --silent http://$DOMAIN/ | grep -q "WordPress"
+WPINDEX=$(echo $?)
+}
+
+function WORDPRESSRESULT {
+if [ $STATUSCODE == "200" ] || [ $WPLIC == "0" ] || [ $WPINDEX == "0" ]
+        then
+                echo -e "\e[32mWordPress\c" && echo -e "\e[39m has been detected." && echo
+
+        fi
+}
+
+# Script
+
+# Clears the screen for a clean working area
+echo -e "\f"
+
+if [ `wc -c domains.txt 2>/dev/null | awk '{print $1}'` -gt '5' ] 2>/dev/null
+then
+
+	echo -e "A list of domains has been detected in \e[32mdomains.txt\e[39m" && echo && sleep 2
+
+	echo "=====================================================" && echo
+
+	while read DOMAIN
+	do
+
+		
+		echo "Testing: $DOMAIN ..." && echo
+
+		# To understand the functions below see the functions toward the top of this file
+
+		DNSTESTS 
+
+		SCODELOOKUP
+
+		LOOKUPLIC
+	
+		LOOKUPWPINDEX
+
+		WORDPRESSRESULT
+
+		echo "=====================================================" && echo
+
+done < domains.txt
+
+	echo "Archiving domains.txt file by renaming it to be domains.old ..." && echo && sleep 2
+	mv domains.txt domains.old
+	exit 0
+
+fi
+
+# Prompt the user for the Domain Name to build a report on
+read -p "Domain Name: " DOMAIN
+
+echo && echo "Analysing $DOMAIN ..." && echo
+
+# To understand the functions below see the functions toward the top of this file
+
+DNSTESTS
+
+SCODELOOKUP
+
+LOOKUPLIC
+
+LOOKUPWPINDEX
+
+WORDPRESSRESULT
+
+exit 0
